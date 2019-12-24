cff5c354b7c0c9ac564b3e80264d4d95eb1d8211
Aaron Burmeister <f34r91@gmail.com>
2019-12-24 23:22:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,3 +1,16 @@
+carty = [
+      {:item => "AVOCADO", :price => 3.00, :clearance => true},
+      {:item => "AVOCADO", :price => 3.00, :clearance => true},
+      {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+      {:item => "ALMONDS", :price => 9.00, :clearance => false},
+      {:item => "TEMPEH", :price => 3.00, :clearance => true},
+      {:item => "CHEESE", :price => 6.50, :clearance => false},
+      {:item => "BEER", :price => 13.00, :clearance => false},
+      {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+      {:item => "BEETS", :price => 2.50, :clearance => false},
+      {:item => "SOY MILK", :price => 4.50, :clearance => true}
+    ]
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   
