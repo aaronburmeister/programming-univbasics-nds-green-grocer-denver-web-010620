8fab4451e0a8ed0ebc7caf2569ac6082bd83a172
Aaron Burmeister <f34r91@gmail.com>
2019-12-25 00:25:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -76,7 +76,7 @@ carty = [
 coupins = [{:item => "AVOCADO", :num => 2, :cost => 5.00}]
 puts carty[0][:count] >= coupins[0][:num]
 test_cart = apply_coupons(carty, coupins)
-
+puts test_cart
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
