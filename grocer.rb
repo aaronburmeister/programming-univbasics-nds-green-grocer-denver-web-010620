776e4aa176c85879fbe53ba4081366d97cd540fa
Aaron Burmeister <f34r91@gmail.com>
2019-12-25 00:21:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,16 +1,3 @@
-carty = [
-      {:item => "AVOCADO", :price => 3.00, :clearance => true},
-      {:item => "AVOCADO", :price => 3.00, :clearance => true},
-      {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
-      {:item => "ALMONDS", :price => 9.00, :clearance => false},
-      {:item => "TEMPEH", :price => 3.00, :clearance => true},
-      {:item => "CHEESE", :price => 6.50, :clearance => false},
-      {:item => "BEER", :price => 13.00, :clearance => false},
-      {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
-      {:item => "BEETS", :price => 2.50, :clearance => false},
-      {:item => "SOY MILK", :price => 4.50, :clearance => true}
-    ]
-
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   
@@ -87,8 +74,7 @@ def apply_coupons(cart, coupons)
   
 end
 carty = [
-  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 3},
-  {:item => "KALE",    :price => 3.00, :clearance => false, :count => 1}
+  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 2}
 ]
 
 coupins = [{:item => "AVOCADO", :num => 2, :cost => 5.00}]
